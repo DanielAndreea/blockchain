3bfd45147e5245a3bef6e79890f46c073a55b37a
Andreea <diandreea1@gmail.com>
2019-12-14 23:43:40 +0200

    initial commit

---
+++ routes/eth.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var router = express.Router();
+var signedTx = require('../bll/signedTransaction');
+var unlockAccount = require('../bll/unlockAccount');
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+router.get('/accounts', function(req,res,next){
+  console.log('provider' , web3.currentProvider);
+  web3.eth.isMining().then(console.log);
+  web3.eth.getAccounts()
+      .then(function(block) {
+        console.log(block);
+        res.send(block);
+      });
+})
+
+router.post('/transaction', function(req,res,next){
+  console.log('provider' , web3.currentProvider);
+  web3.eth.isMining().then(console.log);
+  var acc1 = '0xf49ad8b5B0218d457f7763E212Cc2904E8965d92';
+  var acc2 = '0xFC707311C55b0C3F04B6b461D6849ABC7A235b80';
+  web3.eth.sendTransaction({from: acc1, to: acc2, value: web3.utils.toWei('1','ether')});
+})
+
+router.get('/balance', function(req,res,next){
+  var acc1 = req.body.account;
+  web3.eth.getBalance(acc1, function(err,result) {res.send(result); console.log(result)})
+})
+
+//TUTORIAL: SIGNED TRANSACTIONS
+router.post('/signedTx', signedTx.signedTx);
+
+//SEND TRANSACTION + UNLOCK ACCOUNTS -> local parity node
+router.post('/unlockAccounts',unlockAccount.unlockAccounts );
+
+module.exports = router;
